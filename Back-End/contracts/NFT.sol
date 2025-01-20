bbd42f889b06aae82380a093e1a44d92477fa258
Sharleen Jamoza <sharleenjamoza@gmail.com>
2025-01-20 23:39:19 +0300

    started the contract

--- Back-End/contracts/NFT.sol
+++ Back-End/contracts/NFT.sol
@@ -3,5 +3,11 @@ pragma solidity ^0.8.28;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-
+//functions: event creation
+//minting of the tickets
+//transfer of the tickets
+//burning of the tickets
+//getting the balance of the tickets
+//getting the total supply of the tickets
+//getting the tokenURI of the tickets -- metadata of the ticket
 
